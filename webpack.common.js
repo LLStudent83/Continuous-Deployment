ad50e7424160fcd4b33f8e3fa149bd103ef95f92
Dmitry Prigulnov <prigulnov@mail.ru>
2021-10-10 11:42:55 +0500

    добавил terserPlugin

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,9 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   target: 'web',
-  entry: './src/index.js',
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+},
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
